2881153fd08ffb8b8646da399df64b8d0fa9bb69
tomimandalaputra <tommymandalaputra@gmail.com>
2025-05-14 10:47:54 +0700

    chore: implementation DataManager on top level app

--- MoneyTracker/MoneyTrackerApp.swift
+++ MoneyTracker/MoneyTrackerApp.swift
@@ -9,9 +9,12 @@ import SwiftUI
 
 @main
 struct MoneyTrackerApp: App {
+    let dataManager = DataManager.shared
+
     var body: some Scene {
         WindowGroup {
             HomeView()
+                .environment(\.managedObjectContext, dataManager.container.viewContext)
         }
     }
 }
