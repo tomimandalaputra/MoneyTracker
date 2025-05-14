5ba549a37612ab29224671f4174abda86b7cce2b
tomimandalaputra <tommymandalaputra@gmail.com>
2025-05-14 13:25:31 +0700

    chore: define model data transactionModel use swiftData

--- MoneyTracker/Models/TransactionModel.swift
+++ MoneyTracker/Models/TransactionModel.swift
@@ -6,6 +6,37 @@
 //
 
 import Foundation
+import SwiftData
+
+@Model class TransactionModel {
+    var id: UUID
+    var title: String
+    var type: TransactionType
+    var amount: Double
+    var date: Date
+
+    init(id: UUID, title: String, type: TransactionType, amount: Double, date: Date) {
+        self.id = id
+        self.title = title
+        self.type = type
+        self.amount = amount
+        self.date = date
+    }
+
+    var displayDate: String {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateStyle = .medium
+        return dateFormatter.string(from: date)
+    }
+
+    func displayCurrency(currency: Currency) -> String {
+        let numberFormatter = NumberFormatter()
+        numberFormatter.numberStyle = .currency
+        numberFormatter.locale = currency.locale
+        numberFormatter.maximumFractionDigits = 2
+        return numberFormatter.string(from: NSNumber(value: amount)) ?? ""
+    }
+}
 
 protocol TransactionProtocol {
     var displayDate: String { get }
